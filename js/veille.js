39b8eee54a40f5736bb5b24407fd43a47d8de286
theoo <theooger55@gmail.com>
2025-10-24 14:44:48 +0200

    veille terminer

---
+++ js/veille.js
@@ -0,0 +1,18 @@
+// veille.js : contrôle de l'accordéon
+document.addEventListener("DOMContentLoaded", () => {
+  const toggles = document.querySelectorAll('.accordion-toggle');
+
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      toggle.classList.toggle('active');
+
+      const content = toggle.nextElementSibling;
+
+      if(content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
+});
